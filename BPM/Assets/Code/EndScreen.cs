f9c8147b7ffd2231f75fb169aaa89c24aa9b0155
MateuszSiwekPL <mateusz.siwek.pl@gmail.com>
2024-11-22 12:28:11 +0100

    win lose text

--- BPM/Assets/Code/EndScreen.cs
+++ BPM/Assets/Code/EndScreen.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EndScreen : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _resultText;
     public void Setup(bool win)
     {
-        
+        _resultText.text = win ? "You Win!" : "You Lose!";
     }
     
 }
