f69f027843aae1ee6d2f5bb8fd62e2f75a87c1bc
MateuszSiwekPL <mateusz.siwek.pl@gmail.com>
2024-11-22 14:45:40 +0100

    something

--- BPM/Assets/Code/BonusBehaviour.cs
+++ BPM/Assets/Code/BonusBehaviour.cs
@@ -1,10 +1,43 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BonusBehaviour : MonoBehaviour
 {
-        [SerializeField] private KeyCode _correctKey;
         [SerializeField] private float speed;
         
+        [SerializeField] private GameObject _arrowLeft;
+        [SerializeField] private GameObject _arrowRight;
+        [SerializeField] private GameObject _arrowDown;
+        [SerializeField] private GameObject _arrowUp;
+        
+        private KeyCode _correctKey;
+
+        private void Start()
+        { 
+            KeyCode[] arrowKeys = { KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow };
+            var randomIndex = Random.Range(0, arrowKeys.Length);
+            _correctKey = arrowKeys[randomIndex];
+            
+            switch (_correctKey)
+            {       
+                case KeyCode.LeftArrow:
+                    _arrowLeft.SetActive(true);
+                    break;
+                case KeyCode.RightArrow:
+                    _arrowRight.SetActive(true);
+                    break;
+                case KeyCode.DownArrow:
+                    _arrowDown.SetActive(true);
+                    break;
+                case KeyCode.UpArrow:
+                    _arrowUp.SetActive(true);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
         private void Update()
         {
             transform.Translate(speed * Time.deltaTime * Vector3.down);
