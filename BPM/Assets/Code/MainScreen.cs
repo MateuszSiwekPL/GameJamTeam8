418b00d4d3b053a4a76d3410ff3c5d7b50f36930
MateuszSiwekPL <mateusz.siwek.pl@gmail.com>
2024-11-23 11:18:51 +0100

    tutorial

--- BPM/Assets/Code/MainScreen.cs
+++ BPM/Assets/Code/MainScreen.cs
@@ -11,13 +11,15 @@ public class MainScreen : MonoBehaviour
     [SerializeField] private Button Scene4Button;
     [SerializeField] private Button ExitButton;
     [SerializeField] private Button SceneTutButton;
+
+    [SerializeField] private GameObject _tutorial;
     void Start()
     {
         Scene1Button.onClick.AddListener(() => LoadScene(1));
         Scene2Button.onClick.AddListener(() => LoadScene(2));
         Scene3Button.onClick.AddListener(() => LoadScene(3));
         Scene4Button.onClick.AddListener(() => LoadScene(4));
-        SceneTutButton.onClick.AddListener(() => LoadScene(5));
+        SceneTutButton.onClick.AddListener(() => _tutorial.SetActive(true));
         ExitButton.onClick.AddListener(() => Application.Quit());
     }
     
